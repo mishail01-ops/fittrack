6143abd6f8b307a34626a35a65bd663546ae0172
mishail01-ops <mishail01@gmail.com>
2026-01-28 15:14:43 +0400

    Revert "first commit"

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,14 +1,7 @@
 package daysteps
 
 import (
-	"errors"
-	"fmt"
-	"log"
-	"strconv"
-	"strings"
 	"time"
-
-	sc "github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 const (
@@ -19,72 +12,9 @@ const (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-
-	spl := strings.Split(data, ",")
-	var step int
-	var err error
-	var duration time.Duration
-
-	if len(spl) == 2 {
-
-		step, err = strconv.Atoi(spl[0])
-		if err != nil {
-			log.Println(err)
-			return 0, 0, err
-		}
-		if step <= 0 {
-			err := errors.New("Количество шагов должна быть больше нуля")
-			log.Println(err)
-			return 0, 0, err
-		}
-
-		duration, err = time.ParseDuration(spl[1])
-
-		//Отладка
-		//fmt.Printf("_____")
-		//fmt.Println("!!!Длительность: ", duration)
-
-		if err != nil {
-			log.Println(err)
-			return 0, 0, err
-		}
-		if duration <= 0 {
-			err := errors.New("Длительность должна быть больше нуля")
-			log.Println(err)
-			return 0, 0, errors.New("Длительность должна быть больше нуля")
-
-		}
-
-		return step, duration, nil
-	}
-
-	err = errors.New("Неверный формат данных")
-	log.Println(err)
-	return 0, 0, err
-
+	// TODO: реализовать функцию
 }
 
 func DayActionInfo(data string, weight, height float64) string {
-
-	steps, duration, err := parsePackage(data)
-	if err != nil {
-		log.Println(err)
-		fmt.Println("Ошибка DayActionInfo: ", err)
-		return ""
-	}
-
-	distance := float64(steps) * stepLength / mInKm
-
-	wspent, err := sc.WalkingSpentCalories(steps, weight, height, duration)
-	if err != nil {
-		log.Println(err)
-		fmt.Println("Ошибка: ", err)
-		return ""
-	}
-
-	//Количество шагов: 792.
-	//Дистанция составила 0.51 км.
-	//Вы сожгли 221.33 ккал.
-	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", steps, distance, wspent)
-
+	// TODO: реализовать функцию
 }
